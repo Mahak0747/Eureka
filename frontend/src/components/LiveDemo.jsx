a3ac8c3dc17c58c12fc2c35b0d8cb07fe70964c7
Mahak0747 <mahak.goswami2050@gmail.com>
2025-11-11 19:16:40 +0530

    deployment 3

--- frontend/src/components/LiveDemo.jsx
+++ frontend/src/components/LiveDemo.jsx
@@ -21,7 +21,7 @@ export default function App() {
     setResult("Loading...");
     setProbability(null);
 
-    const res = await fetch("http://127.0.0.1:5000/predict", {
+    const res = await fetch("https://frauddetection-jyqu.onrender.com/predict", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(inputs),
